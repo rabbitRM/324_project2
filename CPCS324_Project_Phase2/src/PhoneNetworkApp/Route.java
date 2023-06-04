90c7787d8b98b4c78def152ab1a4d5730b3776c2
rabbitRM <102517158+rabbitRM@users.noreply.github.com>
2023-06-04 11:15:03 +0300

    Update Route.java

--- CPCS324_Project_Phase2/src/PhoneNetworkApp/Route.java
+++ CPCS324_Project_Phase2/src/PhoneNetworkApp/Route.java
@@ -1,25 +1,26 @@
-
 package PhoneNetworkApp;
 
 import GraphFramework.Edge;
-
 import GraphFramework.Vertex;
-public class Route extends Edge{
-  
+
+public class Route extends Edge {
+
     
+    // Defining the constructor and calling the parent class
+    public Route(Vertex source, Vertex target, int weight) {
+        
+        super(source, target, weight);
+
+    }
+
     
-    public Route( Vertex source, Vertex target , int weight) {
-       super ( source , target , weight);
+    //-----------------------------------------------------------------------------
+    @Override
+    // Method to display the information of the length
+    public String displayInfo() {
       
+        return "route length: " + super.getWeight();
+   
     }
-    
-    
-    
-    public String displayInfo(){
-        return "route length: "+super.getWeight() ;
-    }
-
-
-    
 
 }
